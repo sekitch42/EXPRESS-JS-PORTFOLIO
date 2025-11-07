5b00a93b32c76cbc301bc0daf6283dc30483f95e
sekitch42 <sarah.kitchen@ontariotechu.net>
2025-11-07 12:50:20 -0500

    First Commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,11 @@
+/* SPA --> Single Page Application */
+/* Client Side */
+
+// IIFE --> Immediately invoked function expression 
+(function(){
+  function Start()
+  {
+    console.log("App Started");
+  }
+   window.addEventListener("load", Start);
+})()
